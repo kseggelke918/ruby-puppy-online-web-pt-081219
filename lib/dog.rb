a30ce64e3d4135a363b76fcfc62c34ca91b13e67
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-30 18:17:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
   def initialize(name)
     @name = name 
     #@@all << self 
-    Dog.save(self) 
+    self.save  
   end 
   
   
@@ -19,7 +19,7 @@ class Dog
     end 
   end 
   
-  def save(dog) 
-    @@all << dog 
+  def save 
+    @@all << self  
   end 
 end 
